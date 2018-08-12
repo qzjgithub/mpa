9195963fafe1b53b2448685813f5613daf87f4af
小天使 <qzhujun@sina.com>
2018-08-13 00:15:24 +0800

    initial redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory, useRouterHistory, IndexRoute } from 'react-router'
 import App from './App';
 
+const state = window.__initialState__ || undefined;
+const store = configureStore(browserHistory, state);
+
+const routes = (
+    <Route path="/" component={App}></Route>
+);
+
 render(
-    <App />,
+    <Provider store={ store }>
+        <Router routes={ routes } />
+    </Provider>,
     document.getElementById('app')
 );
